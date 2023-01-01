1a93f29ab76f7f852c540b2ac0b9c542dcbbc0d1
Admin <hackimov.edik@yandex.ru>
2023-01-01 19:59:44 +0500

    feat: realize ToxAtkAbility

--- src/main/java/com/hacman/gomunkulevolv/abilities/ToxAtkAbility.java
+++ src/main/java/com/hacman/gomunkulevolv/abilities/ToxAtkAbility.java
@@ -1,19 +1,39 @@
 package com.hacman.gomunkulevolv.abilities;
 
+import com.hacman.gomunkulevolv.game.session.MainGomunkulEvolv;
 import com.hacman.gomunkulevolv.object.Creature;
 
 public class ToxAtkAbility extends Ability{
+    private int toxinDamage = 1;
+
     public ToxAtkAbility(int abilityClass, String title) {
         super(abilityClass, title);
     }
 
     @Override
-    public float onDefense(Creature curCreature, Creature enemy) {
-        return 1;
+    public void onSuccessAttack(Creature creature, Creature enemy) {
+        super.onSuccessAttack(creature, enemy);
+        MainGomunkulEvolv.executorService.submit(() -> intoxicateEnemy(enemy));
+
+    }
+
+    @SuppressWarnings("BusyWait")
+    private void intoxicateEnemy(Creature enemy) {
+        int count = 0;
+        while (enemy.isInBattle() && count < 4){
+            enemy.takeDamage(toxinDamage);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            count++;
+        }
     }
 
     @Override
-    public boolean isAtkSuccess(Creature enemy) {
-        return  true;
+    public void lvlUp() {
+        super.lvlUp();
+        this.toxinDamage *=2;
     }
 }

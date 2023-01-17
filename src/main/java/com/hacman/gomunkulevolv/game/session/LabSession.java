7eb6ec7ac18996024ca7bea07b55ace9af3b3b07
Edward <hackimov.edik@yandex.ru>
2023-01-18 03:22:43 +0500

    fix: start gensCount

--- src/main/java/com/hacman/gomunkulevolv/game/session/LabSession.java
+++ src/main/java/com/hacman/gomunkulevolv/game/session/LabSession.java
@@ -9,7 +9,7 @@ import java.util.Map;
 
 public class LabSession {
     private MainCreature mainCreature;
-    private int gensCount = 150;
+    private int gensCount = 0;
     private HashMap<LabAbilities, LabAbility> curLabAbility = new HashMap<>();
     private final HashMap<LabAbilities, LabAbility> possibleLabAbility = new HashMap<>();
 

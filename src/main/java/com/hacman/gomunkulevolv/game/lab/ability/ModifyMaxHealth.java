6474640dea62346a7d62cd4fdb98716e6e35b163
Edward <hackimov.edik@yandex.ru>
2023-01-18 15:21:57 +0500

    feat: labAbility MaxHealth

--- src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyMaxHealth.java
+++ src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyMaxHealth.java
@@ -1,11 +1,21 @@
 package com.hacman.gomunkulevolv.game.lab.ability;
 
+import com.hacman.gomunkulevolv.object.MainCreature;
+
 public class ModifyMaxHealth extends LabAbility {
     private final LabAbilities id = LabAbilities.MAX_HEALTH;
     public ModifyMaxHealth() {
         this.setClassLabAbility(1);
         this.setTitle(getId().toString());
         this.setCost(100);
+    }
+
+    @Override
+    public void modifyCreature(MainCreature creature) {
+        super.modifyCreature(creature);
+        double modificationMaxHealth = creature.getMaxHealth() + this.getLabAbilityLevel()*50;
+        creature.setMaxHealth(modificationMaxHealth);
+        creature.setCurHealth(modificationMaxHealth);
     }
 
     public LabAbilities getId() {

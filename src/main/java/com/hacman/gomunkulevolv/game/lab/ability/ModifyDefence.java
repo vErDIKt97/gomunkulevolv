2e552f88bf5d0008c06d6afb02a8a251e3a47bb0
Edward <hackimov.edik@yandex.ru>
2023-01-18 19:00:06 +0500

    feat: defence ability

--- src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyDefence.java
+++ src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyDefence.java
@@ -1,11 +1,19 @@
 package com.hacman.gomunkulevolv.game.lab.ability;
 
+import com.hacman.gomunkulevolv.object.MainCreature;
+
 public class ModifyDefence extends LabAbility {
     private final LabAbilities id = LabAbilities.DEFENSE;
     public ModifyDefence() {
         this.setClassLabAbility(1);
         this.setTitle(getId().toString());
         this.setCost(150);
+    }
+
+    @Override
+    public void modifyCreature(MainCreature creature) {
+        super.modifyCreature(creature);
+        creature.setDefence(creature.getDefence() + getLabAbilityLevel()*2);
     }
 
     public LabAbilities getId() {

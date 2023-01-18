29527451e9cc1bbf9dbf06fd0681591aa47388c8
Edward <hackimov.edik@yandex.ru>
2023-01-18 15:12:39 +0500

    feat: labAbility AtkSpeed

--- src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyAtkSpeed.java
+++ src/main/java/com/hacman/gomunkulevolv/game/lab/ability/ModifyAtkSpeed.java
@@ -1,11 +1,20 @@
 package com.hacman.gomunkulevolv.game.lab.ability;
 
+import com.hacman.gomunkulevolv.object.MainCreature;
+
 public class ModifyAtkSpeed extends LabAbility {
     private final LabAbilities id = LabAbilities.ATTACK_SPEED;
     public ModifyAtkSpeed() {
         this.setClassLabAbility(1);
         this.setTitle(getId().toString());
         this.setCost(500);
+    }
+
+    @Override
+    public void modifyCreature(MainCreature creature) {
+        super.modifyCreature(creature);
+        double modificationAtkSpeed = creature.getAtkSpeed() - this.getLabAbilityLevel()*0.2;
+        creature.setAtkSpeed(modificationAtkSpeed);
     }
 
     public LabAbilities getId() {
